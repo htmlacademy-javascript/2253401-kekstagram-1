039531a89fd142d7b8486d8420066a9dfd9fec0a
Keks <keks@htmlacademy.ru>
2023-12-14 00:22:03 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/functions.js
+++ 2/js/functions.js
@@ -1,44 +1,42 @@
 //палиндром
 
-const isGetPalindrome = (string) => {
+const isPalindrome = (string) => {
   const anyString = string.toLowerCase();
 
   let reverseString = '';
   for (let i = anyString.length - 1; i >= 0; i--) {
     reverseString += anyString.at(i);
   }
+
   return anyString === reverseString;
 };
 
-isGetPalindrome('топот');
+isPalindrome('топот');
 
 /* Функция, которая принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа.
 Если в строке нет ни одной цифры, функция должна вернуть NaN:*/
-const pullOut = (string) => {
-  string = string.replace(/\D/g,'');
-  return string;
+const extractNumber = (string) => {
+  string = string.replace(/\D/g,'') || Math.abs (string);
+
+  return Number(string);
 };
 
-pullOut('23gf37');
+extractNumber('23gf37');
 
 //Функция, которая принимает три параметра
-const findThreeParametrs = (string, minLenght, additionalSymbol) => {
-  const lenghtString = minLenght - string.length;
-  if (lenghtString <= 0) {
+const makeNewString = (string, minLength, additionalSymbol) => {
+  const lengthString = minLength - string.length;
+
+  if (lengthString <= 0) {
     return string;
   }
-  return additionalSymbol.slice (0, lenghtString % additionalSymbol.length) +
-additionalSymbol.repeat(lenghtString / additionalSymbol.length) + string;
+
+  return additionalSymbol.slice (0, lengthString % additionalSymbol.length) +
+additionalSymbol.repeat(lengthString / additionalSymbol.length) + string;
 };
-findThreeParametrs('1', 4, '0');
+makeNewString('1', 4, '0');
 
 //Проверка длины
-const isLengthCheck = (string, length) => {
-  if (string.length <= length) {
-    return true;
-  } else {
-    return false;
-  }
-};
+const checkStringLength = (string, length) => string.length <= length;
 
-isLengthCheck('проверяемая строка', 20);
+checkStringLength ('проверяемая строка', 20);
